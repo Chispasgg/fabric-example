30141949718cd5be0bf145fa1509fd4140f22527
Patxi <chispasgg16@gmail.com>
2023-01-09 15:44:22 +0100

    explorer metido con lanzador

---
+++ explorer/lanzar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo cp -r ../test-network/organizations ./org_tmp
+sudo chown -R chispas:chispas ./org_tmp
+mkdir organizations
+mv org_tmp/ordererOrganizations org_tmp/peerOrganizations organizations
+mv organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/*_sk  organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/priv_sk
+rm -r org_tmp
+docker-compose up
