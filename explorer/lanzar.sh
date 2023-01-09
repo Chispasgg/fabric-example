bbc000e2920e7812d992a46a3c5519ba0b5d8e00
Patxi <chispasgg16@gmail.com>
2023-01-09 15:57:14 +0100

    optimizado el explorer

--- explorer/lanzar.sh
+++ explorer/lanzar.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo rm -r organizations
 sudo cp -r ../test-network/organizations ./org_tmp
 sudo chown -R chispas:chispas ./org_tmp
 mkdir organizations
@@ -6,3 +7,4 @@ mv org_tmp/ordererOrganizations org_tmp/peerOrganizations organizations
 mv organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/*_sk  organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/priv_sk
 rm -r org_tmp
 docker-compose up
+rm -r organizations
